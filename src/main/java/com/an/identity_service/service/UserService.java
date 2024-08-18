aa887d4206f555b811b8fc35041a32b9c70d328a
adohong4 <adohong4@gmail.com>
2024-08-18 16:18:32 +0700

    CRUD basic

--- src/main/java/com/an/identity_service/service/UserService.java
+++ src/main/java/com/an/identity_service/service/UserService.java
@@ -1,10 +1,13 @@
 package com.an.identity_service.service;
 
 import com.an.identity_service.dto.request.UserCreationRequest;
+import com.an.identity_service.dto.request.UserUpdateRequest;
 import com.an.identity_service.entity.User;
 import com.an.identity_service.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
@@ -22,5 +25,30 @@ public class UserService {
 
      return userRepository.save(user);
     }
+
+    public User updateUser(String userId, UserUpdateRequest request){
+        User user = getUser(userId);
+
+        user.setPassword(request.getPassword());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setDob(request.getDob());
+
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(String userId) {
+        userRepository.deleteById(userId);
+    }
+
+    public List<User> getUsers(){
+        return  userRepository.findAll();
+    }
+
+    public User getUser(String id){
+        return  userRepository.findById(id).orElseThrow(() -> new RuntimeException("User Not found"));
+    }
+
+
 }
 
