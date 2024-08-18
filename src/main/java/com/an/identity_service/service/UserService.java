e7daa168e733da02d3d35040f8d107d573ee45ef
adohong4 <adohong4@gmail.com>
2024-08-18 23:58:28 +0700

    authentication & verify

--- src/main/java/com/an/identity_service/service/UserService.java
+++ src/main/java/com/an/identity_service/service/UserService.java
@@ -12,6 +12,8 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -28,14 +30,16 @@ public class UserService {
         throw new AppException(ErrorCode.USER_EXISTED);
 
      User user = userMapper.toUser(request);
+     PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(10);
+     user.setPassword(passwordEncoder.encode(request.getPassword()));
 
      return userRepository.save(user);
     }
 
     public UserResponse updateUser(String userId, UserUpdateRequest request){
         User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("User Not found"));
-        userMapper.updateUser(user, request);
 
+        userMapper.updateUser(user, request);
         return userMapper.toUserResponse(userRepository.save(user));
     }
 
