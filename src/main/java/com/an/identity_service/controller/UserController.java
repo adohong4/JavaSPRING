c8c7c3c7a642c21ec7fd8381c47953602dfb7380
adohong4 <adohong4@gmail.com>
2024-08-18 23:12:02 +0700

    mapstructor && update code

--- src/main/java/com/an/identity_service/controller/UserController.java
+++ src/main/java/com/an/identity_service/controller/UserController.java
@@ -3,9 +3,13 @@ package com.an.identity_service.controller;
 import com.an.identity_service.dto.request.ApiResponse;
 import com.an.identity_service.dto.request.UserCreationRequest;
 import com.an.identity_service.dto.request.UserUpdateRequest;
+import com.an.identity_service.dto.response.UserResponse;
 import com.an.identity_service.entity.User;
 import com.an.identity_service.service.UserService;
 import jakarta.validation.Valid;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,8 +17,9 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/users")
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class UserController {
-    @Autowired
     private UserService userService;
 
     @PostMapping
@@ -32,12 +37,12 @@ public class UserController {
     }
 
     @GetMapping("/{userId}")
-    User getUser(@PathVariable("userId") String userId) {
+    UserResponse getUser(@PathVariable("userId") String userId) {
         return userService.getUser(userId);
     }
 
     @PutMapping("/{userId}")
-    User updateUser(@PathVariable String userId, @RequestBody UserUpdateRequest request) {
+    UserResponse updateUser(@PathVariable String userId, @RequestBody UserUpdateRequest request) {
         return userService.updateUser(userId, request);
     }
 
