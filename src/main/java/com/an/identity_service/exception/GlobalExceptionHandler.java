32f1915bf5ffed9b834eda766c497f69df6281ec
adohong4 <adohong4@gmail.com>
2024-08-18 16:29:56 +0700

    handler exception

---
+++ src/main/java/com/an/identity_service/exception/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.an.identity_service.exception;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value =  RuntimeException.class)
+    ResponseEntity<String> handlingRuntimeException(RuntimeException exception) {
+        return ResponseEntity.badRequest().body(exception.getMessage());
+    }
+}
