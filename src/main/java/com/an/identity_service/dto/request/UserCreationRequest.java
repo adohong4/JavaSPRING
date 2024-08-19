76b6532d265082c3d075ac28f9b6dac2b3bf0491
adohong4 <adohong4@gmail.com>
2024-08-19 22:21:45 +0700

    validator && custom Annottion

--- src/main/java/com/an/identity_service/dto/request/UserCreationRequest.java
+++ src/main/java/com/an/identity_service/dto/request/UserCreationRequest.java
@@ -22,6 +22,6 @@ public class UserCreationRequest {
     String firstName;
     String lastName;
 
-    @DobConstraint(min = 18)
+    @DobConstraint(min = 18, message = "INVALID_DOB")
     LocalDate dob;
 }
--- src/main/java/com/an/identity_service/dto/request/UserUpdateRequest.java
+++ src/main/java/com/an/identity_service/dto/request/UserUpdateRequest.java
@@ -1,5 +1,6 @@
 package com.an.identity_service.dto.request;
 
+import com.an.identity_service.validator.DobConstraint;
 import lombok.*;
 import lombok.experimental.FieldNameConstants;
 import java.time.LocalDate;
@@ -15,6 +16,8 @@ public class UserUpdateRequest {
     String password;
     String firstName;
     String lastName;
+
+    @DobConstraint(min = 8, message = "INVALID_DOB")
     LocalDate dob;
     List<String> roles;
 }
