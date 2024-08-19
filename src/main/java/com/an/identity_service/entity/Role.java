6cf5836c727eae07b90dfee25458ffbf1dcebafb
adohong4 <adohong4@gmail.com>
2024-08-19 17:11:57 +0700

    Role && Permission

--- src/main/java/com/an/identity_service/entity/Role.java
+++ src/main/java/com/an/identity_service/entity/Role.java
@@ -14,15 +14,11 @@ import java.util.Set;
 @AllArgsConstructor
 @FieldDefaults(level =  AccessLevel.PRIVATE)
 @Entity
-public class User {
+public class Role {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
-    private String username;
-    private String password;
-    private String firstName;
-    private String lastName;
-    private LocalDate dob;
+    String name;
+    String description;
 
-    Set<String> roles;
+    @ManyToMany
+    Set<Permission> permissions;
 }
---
+++ src/main/java/com/an/identity_service/mapper/PermissionMapper.java
@@ -0,0 +1,18 @@
+package com.an.identity_service.mapper;
+
+import com.an.identity_service.dto.request.PermissionRequest;
+import com.an.identity_service.dto.request.UserCreationRequest;
+import com.an.identity_service.dto.request.UserUpdateRequest;
+import com.an.identity_service.dto.response.PermissionResponse;
+import com.an.identity_service.dto.response.UserResponse;
+import com.an.identity_service.entity.Permission;
+import com.an.identity_service.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+
+@Mapper(componentModel = "spring")
+public interface PermissionMapper {
+    Permission toPermission(PermissionRequest request);
+
+    PermissionResponse toPermissionResponse(Permission permission);
+}
